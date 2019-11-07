dc8de1bbf744ef48820f1eb36b23494e2c1f8fc7
Matteo Borghesi <ma.borghe@gmail.com>
2019-11-07 15:45:02 +0100

    Plot SVM

--- hw1.py
+++ hw1.py
@@ -23,6 +23,7 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, .02),
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
+"""
 # 1. KNN
 K = [1,3,5,7]
 Kscore = [None]*len(K)
@@ -52,7 +53,14 @@ while i < len(K):
     i += 1
 
 plt.show()
+#plt.savefig('KnnPredPlot')
 
+# plot Kscores
+plt.figure()
+plt.scatter(K, Kscore)
+plt.show()
+#plt.savefig('KnnscorePlot')
+"""
 
 """
 # print out predictions
@@ -63,41 +71,51 @@ print(*yVal)
 print(*Kscore)
 
 """
-# plot Kscores
-plt.figure()
-plt.scatter(K, Kscore)
-plt.show()
-#plt.savefig('KscorePlot')
-"""
-
 
 # 2. SVM
 C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
 SVMscore = [None]*len(C)
 SVMpred = [None]*len(C)
 i = 0
+plt.figure()
+plt.subplots_adjust(hspace=0.4, wspace=1.5)
 while i < len(C):
     clf = SVC(C=C[i], gamma='scale').fit(xTrain, yTrain)
     SVMpred[i] = clf.predict(xVal)
     SVMscore[i] = clf.score(xVal, yVal)
-    plt.figure()
-    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold,
-                edgecolor='k', s=20)        
+    plt.subplot(3, 3, i+1)    
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
-    plt.title("Wine SVM (C = %g)"
-              % (C[i]))
-    plt.show()
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=cmap_light)
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)        
+    #plt.title("(C = %g)"
+    #        % (C[i]))
+    
     i += 1
+    
+plt.show()
+plt.savefig('SvmPredPlot')
 
+# plot Kscores
+plt.figure()
+plt.xscale("log")
+plt.scatter(C, SVMscore)
+plt.show()
+plt.savefig('SvmScorePlot')
+
+"""
 # print out predictions
 for line in SVMpred:
     print(*line)
 print('------')
 print(*yVal)
 print(*SVMscore)
-
 """
+
+
 
 
 
