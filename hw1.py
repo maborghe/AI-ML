0d01c05c8c8be8defd468058b827fe6b1b112027
Matteo Borghesi <ma.borghe@gmail.com>
2019-11-07 15:51:45 +0100

    Add linear kernel

--- hw1.py
+++ hw1.py
@@ -71,40 +71,40 @@ print(*yVal)
 print(*Kscore)
 
 """
-
-# 2. SVM
-C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
-SVMscore = [None]*len(C)
-SVMpred = [None]*len(C)
-i = 0
-plt.figure()
-plt.subplots_adjust(hspace=0.4, wspace=1.5)
-while i < len(C):
-    clf = SVC(C=C[i], gamma='scale').fit(xTrain, yTrain)
-    SVMpred[i] = clf.predict(xVal)
-    SVMscore[i] = clf.score(xVal, yVal)
-    plt.subplot(3, 3, i+1)    
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    plt.contourf(xx, yy, Z, cmap=cmap_light)
-    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold,
-                edgecolor='k', s=20)        
-    #plt.title("(C = %g)"
-    #        % (C[i]))
+for c, ker in enumerate(["linear", "rbf"]):
+    # 2. SVM
+    C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    SVMscore = [None]*len(C)
+    SVMpred = [None]*len(C)
+    i = 0
+    plt.figure()
+    plt.subplots_adjust(hspace=0.4, wspace=1.5)
+    while i < len(C):
+        clf = SVC(C=C[i], gamma='scale', kernel=ker).fit(xTrain, yTrain)
+        SVMpred[i] = clf.predict(xVal)
+        SVMscore[i] = clf.score(xVal, yVal)
+        plt.subplot(3, 3, i+1)    
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap=cmap_light)
+        plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold,
+                    edgecolor='k', s=20)        
+        #plt.title("(C = %g)"
+        #        % (C[i]))
+        
+        i += 1
+        
+    plt.show()
+    plt.savefig('SvmPredPlot')
     
-    i += 1
-    
-plt.show()
-plt.savefig('SvmPredPlot')
-
-# plot Kscores
-plt.figure()
-plt.xscale("log")
-plt.scatter(C, SVMscore)
-plt.show()
-plt.savefig('SvmScorePlot')
+    # plot Kscores
+    plt.figure()
+    plt.xscale("log")
+    plt.scatter(C, SVMscore)
+    plt.show()
+    plt.savefig('SvmScorePlot')
 
 """
 # print out predictions
